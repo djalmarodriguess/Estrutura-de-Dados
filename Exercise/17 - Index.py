0af786619ff7e467dd039512a8bdf14ed36d2582
djalmarodriguess <djalmarodrigues1206@outlook.com>
2022-02-15 20:52:28 -0300

    exercices

---
+++ Exercise/17 - Index.py
@@ -0,0 +1,7 @@
+# Write a Python program access the index of a list
+
+lista = [15, 26, 37, 48, 59, 63, 74, 85]
+
+for i in enumerate(lista):
+    print(i)
+
---
+++ Exercise/18 - Convert a list in a string.py
@@ -0,0 +1,6 @@
+#Write a Python program to convert a list of characters into a string
+
+lista = ['P', 'y', 't', 'h', 'o', 'n']
+
+j = ''.join(lista)
+print(j)
